afbd09ce88385475ff1a2216a93cae0a003b58a5
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-07-16 15:34:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ class Dog extends Animal {
 	bark(){
 		console.log("woof");
 	}
+	 makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+    this.bark();
+  }
 }
 
 class Cat extends Animal {
@@ -30,6 +34,11 @@ class Cat extends Animal {
 	purr(){
 		console.log("purr");
 	}
+
+	 makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+    this.purr();
+  }
 }
 
 // Do not change the code below this line
